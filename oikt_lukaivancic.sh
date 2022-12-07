cbc985da837342ae387b86ffa4d2e73028d26c95
lukaivancic14 <110102950+lukaivancic14@users.noreply.github.com>
2022-12-07 12:20:36 +0100

    Update oikt_lukaivancic.sh

--- oikt_lukaivancic.sh
+++ oikt_lukaivancic.sh
@@ -1,31 +1,50 @@
-#!/bin/sh
+#!/bin/bash
 mkdir -p Struktura/{Desktop,Documents,Downloads,Pictures,Videos}
-for $i in {1...5}
+
+for i in {1..5}
 do
-	sudo mkdir "mapa$i"
+    sudo mkdir /folder$i
 done
-for $i in {1...5}
+wait
+for i in ena dva tri stiri pet
 do
-	sudo useradd "uporabnik$i"
-	sudo usermod -aG sudo uporabnik$i
+    sudo adduser user$i 
+    wait
+    sudo usermod -aG sudo user$i
 done
+wait
 sudo apt-get update
+wait
 sudo apt-get upgrade
-sudo apt-get update
-sudo apt-get install nginx
+wait
+
+sudo apt install nginx
+wait
 systemctl status nginx
-sudo apt-get install git
-sudo apt-get install net-tools
+wait
+
+sudo apt install net-tools
+wait
+
+sudo apt install git
+wait
+
 sudo apt-get update
-sudo apt-get install\
-   ca-certificates\
-   curl\
-   gnupg\
-   lsb-release
+wait
+
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+wait
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+wait
 echo \
-   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg]https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+wait
 sudo apt-get update
+wait
 sudo snap install docker
