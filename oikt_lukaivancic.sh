f21d793b139b4e7c749f716de3681ffefbde0a87
lukaivancic14 <110102950+lukaivancic14@users.noreply.github.com>
2022-12-07 12:06:02 +0100

    Add files via upload

--- oikt_lukaivancic.sh
+++ oikt_lukaivancic.sh
@@ -2,12 +2,12 @@
 
 mkdir -p Struktura/{Desktop, Documents, Downloads, Pictures, Videos}
 
-for i in {1...5}
+for i in{1...5}
 do
    mkdir "mapa$i"
 done
 
-for i in {1...5}
+for i in{1...5}
 do
    sudo useradd "uporabnik$i"
    sudo usermod -aG sudo uporabnik$i
